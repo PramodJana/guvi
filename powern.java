27f79775ac1133d4d093933494ee77020e9ccc85
Pramodjana <Pramodjana395@gmail.com>
2019-03-14 18:32:35 +0530

    power of a number

---
+++ powern.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+class powern
+{
+	public static void main(String[] args)
+	{
+		Scanner kb=new Scanner(System.in);
+		int m,n;
+		m=kb.nextInt();
+		n=kb.nextInt();
+		System.out.println((int)Math.pow(m,n));
+	}
+}
