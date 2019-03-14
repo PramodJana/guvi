8e91fad26a62ff94889259fb30e37a51048574d6
Pramodjana <Pramodjana395@gmail.com>
2019-03-14 21:21:05 +0530

    appending fullstop in the string

---
+++ appendstop.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.io.*;
+class appendstop
+{
+	public static void main(String [] args)
+	{
+		Scanner kb= new Scanner(System.in);
+		String str=kb.nextLine();
+		System.out.println(str+".");
+	}
+}
