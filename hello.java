23c2b59cbb8ec4480f0cfb70afc9ef930f4734c5
Pramodjana <Pramodjana395@gmail.com>
2019-03-14 11:42:47 +0530

    multiple hello

---
+++ hello.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+class hello
+{
+	public static void main(String[] args)
+	{
+		Scanner kb = new Scanner(System.in);
+		int n=kb.nextInt();
+		for(int i=0;i<n;i++)
+		{
+			System.out.println("Hello");
+		}
+	}
+}
