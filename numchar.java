9ca4b8a8e123707bffe842488192a0172c1c7e7e
Pramodjana <Pramodjana395@gmail.com>
2019-03-28 17:24:48 +0530

    counting number of characters

---
+++ numchar.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+class numchar
+{
+	public static void main(String[] args)
+	{
+		Scanner kb = new Scanner(System.in);
+		String str=kb.nextLine();
+		int len=str.length();
+		int count=0;
+		for(int i=0;i<len;i++)
+		{
+			if(str.charAt(i)!=' ')
+			{
+				count++;
+			}
+		}
+		System.out.println(count);
+	}
+}
