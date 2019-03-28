27ed7505d8936fc264b9e688dca3a9428a758c57
Pramodjana <Pramodjana395@gmail.com>
2019-03-28 17:35:42 +0530

    counting number of lines

---
+++ numlines.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+class numlines
+{
+	public static void main(String[] args)
+	{
+		Scanner kb = new Scanner(System.in);
+		String str=kb.nextLine();
+		int len=str.length();
+		int count=0;
+		if(len<=1000){
+		for(int i=0;i<len;i++)
+		{
+			if(str.charAt(i)=='.')
+			{
+				count++;
+			}
+		}
+		System.out.println(count+1);
+	}}
+}
