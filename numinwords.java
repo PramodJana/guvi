7f32c8c54efe45c4678bcc717768e27474623fa0
Pramodjana <Pramodjana395@gmail.com>
2019-03-31 16:53:46 +0530

    printing number in words

---
+++ numinwords.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+class numinwords
+{
+	public static void main(String[] args)
+	{
+		Scanner kb=new Scanner(System.in);
+		int num=kb.nextInt();
+		switch(num)
+		{
+			case 1:
+			System.out.println("One");
+			break;
+			case 2:
+			System.out.println("Two");
+			break;
+			case 3:
+			System.out.println("Three");
+			break;
+			case 4:
+			System.out.println("Four");
+			break;
+			case 5:
+			System.out.println("Five");
+			break;
+			case 6:
+			System.out.println("Six");
+			break;
+			case 7:
+			System.out.println("Seven");
+			break;
+			case 8:
+			System.out.println("Eight");
+			break;
+			case 9:
+			System.out.println("Nine");
+			break;
+			case 10:
+			System.out.println("Ten");
+		}
+	}
+}
