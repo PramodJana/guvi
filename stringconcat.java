40711b0d48d5e7d148ec82a41bac90e0b5741d67
Pramodjana <Pramodjana395@gmail.com>
2019-03-31 11:02:22 +0530

    concatinating two strings without concat operator

---
+++ stringconcat.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+class stringconcat
+{
+	public static void main(String[] args)
+	{
+		Scanner kb=new Scanner(System.in);
+		String str=kb.next();
+		String str1=kb.next();
+		System.out.println(str+str1);
+	}
+}
