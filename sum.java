40f59ce5398c7d5632292de632283ee65ba284a4
Pramodjana <Pramodjana395@gmail.com>
2019-03-31 16:22:26 +0530

    sum of two numbers

---
+++ sum.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+class sum
+{
+	public static void main(String[] args)
+	{
+		Scanner kb= new Scanner(System.in);
+		int a=kb.nextInt();
+		int b=kb.nextInt();
+		System.out.println(a+b);
+	}
+}
