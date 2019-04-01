f3114c3b9a987e474e99b883ebf58bcdb639d6ff
Pramodjana <Pramodjana395@gmail.com>
2019-04-02 00:22:12 +0530

    finding number is between two number or not

---
+++ between.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+class between
+{
+	public static void main(String[] args)
+	{
+		Scanner kb=new Scanner(System.in);
+		int a=kb.nextInt();
+		int b=kb.nextInt();
+		int c=kb.nextInt();
+		if(a>b&&a<c)
+		System.out.println("yes");
+		else
+		System.out.println("no");
+	}
+}
