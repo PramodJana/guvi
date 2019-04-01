ab870b338353ac4ed371c07d6295676099c624b4
Pramodjana <Pramodjana395@gmail.com>
2019-04-01 14:12:57 +0530

    finding nearest greater power of 2

---
+++ nearpow2.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.math.*;
+import java.util.*;
+class nearpow2
+{
+	public static void main(String[] args)
+	{
+		Scanner kb= new Scanner(System.in);
+		int num=kb.nextInt();
+		System.out.println(num*2);
+	}
+}
