f0b63735cf31b118bbf64afa3d8941fe999ce888
Pramodjana <Pramodjana395@gmail.com>
2019-04-01 11:49:51 +0530

    printing statement without semicolon

---
+++ semicolon.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+public class semicolon 
+{
+     public static void main(String[] args) 
+     {
+	Scanner kb=new Scanner(System.in);
+	String str=kb.next(); 
+        int i = 0;
+       	for(i = 1; i < 2; System.out.println(str))
+        {
+            i++;
+     	}
+     }
+}
