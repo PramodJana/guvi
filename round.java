36891ffa84e4bbb010b849e4c33d070c19b60649
Pramodjana <Pramodjana395@gmail.com>
2019-04-02 00:27:52 +0530

    finding round off of a number

---
+++ round.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+class round
+{
+	public static void main(String[] args)
+	{
+		Scanner kb=new Scanner(System.in);
+		double db=kb.nextDouble();
+		if((int)db<db)
+		System.out.println((int)db+1);
+	}
+}
