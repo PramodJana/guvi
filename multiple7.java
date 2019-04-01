b1e57302750411806a96b21b330f534162499b9d
Pramodjana <Pramodjana395@gmail.com>
2019-04-01 14:02:18 +0530

    finding number is a multiple of 7 or not

---
+++ multiple7.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+class multiple7
+{
+	public static void main(String[] args)
+	{
+		Scanner kb=new Scanner(System.in);
+		int num=kb.nextInt();
+		if(num%7==0)
+		System.out.println("yes");
+		else
+		System.out.println("no");
+	}
+}
